c631960c8764e0504887b1b792f0d2ad242d553c
pneela-vi <pavan.neela@virtualinstruments.com>
2023-03-15 00:07:55 +0530

    Revert "initial commit"
    This reverts commit 06a64e57b645aa74a2ee03dc78efb533a7f256d6.

--- src/main/java/com/virtana/slackapp/service/CommandHandler.java
+++
@@ -1,7 +0,0 @@
-package com.virtana.slackapp.service;
-
-import java.io.IOException;
-
-public interface CommandHandler {
-    public String ipmDashboard() throws IOException, InterruptedException;
-}
